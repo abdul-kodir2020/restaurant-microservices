16c6e07ba19c6642726dbdd59df7348208e74000
abdul-kodir2020 <62393812+abdul-kodir2020@users.noreply.github.com>
2024-11-15 14:39:38 +0000

    connect app to rabbitmq

--- utilisateur/index.js
+++ utilisateur/index.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+
 const port = process.env.PORT || 3000
 
 const connectDB = async () =>{
@@ -16,12 +17,47 @@ const connectDB = async () =>{
     })
 }
 
+let channel, connection;
+
+async function connectRabbitMQ() {
+    try {
+        // Établit la connexion à RabbitMQ
+        connection = await amqp.connect(process.env.RABBITMQ_URL);
+        channel = await connection.createChannel();
+
+        // S'assure que la file d'attente "tasks" existe
+        await channel.assertQueue('tasks');
+        console.log('Connected to RabbitMQ');
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+    }
+}
+
+// app.post('/send', async (req, res) => {
+//     const { message } = req.body;
+  
+//     if (!message) {
+//       return res.status(400).send('Message is required');
+//     }
+  
+//     try {
+//       channel.sendToQueue('tasks', Buffer.from(message));
+//       console.log(`Message sent to RabbitMQ: ${message}`);
+//       res.send('Message sent to RabbitMQ');
+//     } catch (error) {
+//       console.error('Error sending message to RabbitMQ:', error);
+//       res.status(500).send('Failed to send message');
+//     }
+// });
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World for auth')
 })
 
-
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`listening on port ${port}`)
+//   await connectRabbitMQ();
 })
 connectDB()
